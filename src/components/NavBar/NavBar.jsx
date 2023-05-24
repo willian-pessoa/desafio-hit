3eea5cace9b4b3a044bab36218f27a4f5cb64735
willian-pessoa <willianpessoadeabreu@gmail.com>
2023-05-24 17:44:24 -0300

    adicionado navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { AppBar, Toolbar, IconButton, Button } from "@mui/material";
+import { Menu } from "@mui/icons-material";
+
+import FlexBetween from "components/CustomBoxs/FlexBetween";
+
+import { useDispatch } from "react-redux";
+import { setLogout } from "redux/global";
+
+const NavBar = ({ setIsSideBarOpen }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: "antiquewhite", color: "black", padding: "0" }}
+    >
+      <Toolbar>
+        <FlexBetween width="100%">
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            {" "}
+            <Menu onClick={() => setIsSideBarOpen((prev) => !prev)} />
+          </IconButton>
+          <Button onClick={() => dispatch(setLogout())} color="inherit">
+            Logout
+          </Button>
+        </FlexBetween>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
--- src/scenes/Layout/index.jsx
+++ src/scenes/Layout/index.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+
+import NavBar from "components/NavBar/NavBar";
+import SideBar from "components/SideBar/SideBar";
 
 const Layout = () => {
-  return <div></div>;
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+
+  return (
+    <Box
+      display={isNonMobile ? "flex" : "block"}
+      width="100vw"
+      minHeight="100vh"
+    >
+      <SideBar
+        isNonMobile={isNonMobile}
+        isSideBarOpen={isSideBarOpen}
+        setIsSideBarOpen={setIsSideBarOpen}
+      />
+      <Box flexGrow={1}>
+        <NavBar
+          isSideBarOpen={isSideBarOpen}
+          setIsSideBarOpen={setIsSideBarOpen}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+  );
 };
 
 export default Layout;
