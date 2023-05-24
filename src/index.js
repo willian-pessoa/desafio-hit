137b4fc0216f3bf48cee00fd0fed6d54bf559304
willian-pessoa <willianpessoadeabreu@gmail.com>
2023-05-24 15:27:33 -0300

    configurado redux persist

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import './index.css';
 import App from './App';
 
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import store from 'redux/store';
+
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,35 @@
+import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 import { configureStore } from '@reduxjs/toolkit';
 import globalReducer from "redux/global"
+
+const persistConfig = {
+  key: 'global',
+  storage,
+};
 
 const reducers = combineReducers({
   global: globalReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export default configureStore({
-  reducer: reducers,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
